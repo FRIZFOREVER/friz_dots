23df7392339b0addbb60fe609e7b0f029aa3dff7
Kirill Khromov <kirillhromov@mail.ru>
2026-01-05 13:58:11 +0300

    added .gitignore and .zshrc that was copied other way

---
+++ nvim/lua/community.lua
@@ -0,0 +1,9 @@
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.color.transparent-nvim" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.editing-support.conform-nvim" },
+  { import = "astrocommunity.pack.python-ruff" },
+}
