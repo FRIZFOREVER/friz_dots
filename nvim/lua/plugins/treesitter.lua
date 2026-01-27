eecd9e7544c40f7ee7e5862fc45a5eab18b63bd9
Kirill Khromov <kirillhromov@mail.ru>
2026-01-27 16:36:21 +0300

    initial commit

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,14 @@
+---@type LazySpec
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "lua",
+      "vim",
+      "python",
+      "yaml",
+      "go",
+      "dockerfile",
+    },
+  },
+}
